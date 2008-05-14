138208fa6768fb41df5d08ff1cf303abe578a695
Jen Bourey <jennifer.bourey@gmail.com>
2008-05-14 19:42:56 +0000

    NOJIRA Adding user-configurable timezone support.
    git-svn-id: https://source.jasig.org/portlets/CalendarPortlet/trunk@15701 f5dbab47-78f9-eb45-b975-e544023573eb

---
+++ src/main/java/edu/yale/its/tp/portlets/calendar/mvc/CalendarPreferencesCommand.java
@@ -0,0 +1,15 @@
+package edu.yale.its.tp.portlets.calendar.mvc;
+
+public class CalendarPreferencesCommand {
+
+	private String timezone;
+
+	public String getTimezone() {
+		return timezone;
+	}
+
+	public void setTimezone(String timezone) {
+		this.timezone = timezone;
+	}
+	
+}
