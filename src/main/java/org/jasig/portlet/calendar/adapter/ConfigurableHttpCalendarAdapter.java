02a80b24541bb21811604bf378b2baac485e7e3d
Drew Wills <wills.drew@gmail.com>
2016-07-18 18:08:56 -0700

    Clean up whitespace (and one small javadoc item) based on uPortal community style guidelines

--- src/main/java/org/jasig/portlet/calendar/adapter/ConfigurableHttpCalendarAdapter.java
+++ src/main/java/org/jasig/portlet/calendar/adapter/ConfigurableHttpCalendarAdapter.java
@@ -51,7 +51,6 @@ import org.jasig.portlet.calendar.url.IUrlCreator;
 import org.joda.time.Interval;
 import org.springframework.beans.factory.annotation.Required;
 
-
 /**
  * Implementation of {@link ICalendarAdapter} that uses Commons HttpClient
  * for retrieving {@link CalendarEventSet}s.
@@ -71,76 +70,75 @@ import org.springframework.beans.factory.annotation.Required;
  * a shared secret, or behind CAS.
  * 
  * @author Nicholas Blair, nblair@doit.wisc.edu
- * @version $Header: RefactoredHttpICalendarAdapter.java Exp $
  */
 public final class ConfigurableHttpCalendarAdapter<T> extends AbstractCalendarAdapter implements ICalendarAdapter {
 
-	protected final Log log = LogFactory.getLog(this.getClass());
-	
-	private Cache cache;
-	private IUrlCreator urlCreator = new DefaultUrlCreatorImpl();
-	private ICredentialsExtractor credentialsExtractor = new DefaultCredentialsExtractorImpl();
-	private IContentProcessor contentProcessor = new ICalendarContentProcessorImpl();
-	private ICacheKeyGenerator cacheKeyGenerator = new DefaultCacheKeyGeneratorImpl();
-	private String cacheKeyPrefix = "default";
-	
-	/**
-	 * @param cache the cache to set
-	 */
-	@Required
-	public void setCache(Cache cache) {
-		this.cache = cache;
-	}
-	
-	/**
-	 * @param urlCreator the urlCreator to set
-	 */
-	public void setUrlCreator(IUrlCreator urlCreator) {
-		this.urlCreator = urlCreator;
-	}
-	
-	/**
-	 * @param credentialsExtractor the credentialsExtractor to set
-	 */
-	public void setCredentialsExtractor(ICredentialsExtractor credentialsExtractor) {
-		this.credentialsExtractor = credentialsExtractor;
-	}
+    protected final Log log = LogFactory.getLog(this.getClass());
 
-	/**
-	 * @param contentProcessor the contentProcessor to set
-	 */
-	public void setContentProcessor(IContentProcessor contentProcessor) {
-		this.contentProcessor = contentProcessor;
-	}
+    private Cache cache;
+    private IUrlCreator urlCreator = new DefaultUrlCreatorImpl();
+    private ICredentialsExtractor credentialsExtractor = new DefaultCredentialsExtractorImpl();
+    private IContentProcessor contentProcessor = new ICalendarContentProcessorImpl();
+    private ICacheKeyGenerator cacheKeyGenerator = new DefaultCacheKeyGeneratorImpl();
+    private String cacheKeyPrefix = "default";
 
-	/**
-	 * @param cacheKeyPrefix the cacheKeyPrefix to set
-	 */
-	public void setCacheKeyPrefix(String cacheKeyPrefix) {
-		this.cacheKeyPrefix = cacheKeyPrefix;
-	}
-	
-	public void setCacheKeyGenerator(ICacheKeyGenerator cacheKeyGenerator) {
-		this.cacheKeyGenerator = cacheKeyGenerator;
-	}
+    /**
+     * @param cache the cache to set
+     */
+    @Required
+    public void setCache(Cache cache) {
+        this.cache = cache;
+    }
 
-	/**
-	 * Workflow for this implementation:
-	 * 
-	 * <ol>
-	 * <li>consult the configured {@link IUrlCreator} for the url to request</li>
-	 * <li>consult the cache to see if the fetch via HTTP is necessary (if not return the cached events)</li>
-	 * <li>if the fetch is necessary, consult the {@link ICredentialsExtractor} for necessary {@link Credentials}</li>
-	 * <li>Invoke retrieveCalendarHttp</li>
-	 * <li>Pass the returned {@link InputStream} into the configured {@link IContentProcessor}</li>
-	 * <li>Return the {@link CalendarEventSet}s</li>
-	 * </ol>
-	 * 
-	 *  (non-Javadoc)
-	 * @see org.jasig.portlet.calendar.adapter.ICalendarAdapter#getEvents(org.jasig.portlet.calendar.CalendarConfiguration, org.joda.time.Interval, javax.portlet.PortletRequest)
-	 */
-	public CalendarEventSet getEvents(CalendarConfiguration calendarConfiguration,
-			Interval interval, PortletRequest request) throws CalendarException {
+    /**
+     * @param urlCreator the urlCreator to set
+     */
+    public void setUrlCreator(IUrlCreator urlCreator) {
+        this.urlCreator = urlCreator;
+    }
+
+    /**
+     * @param credentialsExtractor the credentialsExtractor to set
+     */
+    public void setCredentialsExtractor(ICredentialsExtractor credentialsExtractor) {
+        this.credentialsExtractor = credentialsExtractor;
+    }
+
+    /**
+     * @param contentProcessor the contentProcessor to set
+     */
+    public void setContentProcessor(IContentProcessor contentProcessor) {
+        this.contentProcessor = contentProcessor;
+    }
+
+    /**
+     * @param cacheKeyPrefix the cacheKeyPrefix to set
+     */
+    public void setCacheKeyPrefix(String cacheKeyPrefix) {
+        this.cacheKeyPrefix = cacheKeyPrefix;
+    }
+
+    public void setCacheKeyGenerator(ICacheKeyGenerator cacheKeyGenerator) {
+        this.cacheKeyGenerator = cacheKeyGenerator;
+    }
+
+    /**
+     * Workflow for this implementation:
+     * 
+     * <ol>
+     * <li>consult the configured {@link IUrlCreator} for the url to request</li>
+     * <li>consult the cache to see if the fetch via HTTP is necessary (if not return the cached events)</li>
+     * <li>if the fetch is necessary, consult the {@link ICredentialsExtractor} for necessary {@link Credentials}</li>
+     * <li>Invoke retrieveCalendarHttp</li>
+     * <li>Pass the returned {@link InputStream} into the configured {@link IContentProcessor}</li>
+     * <li>Return the {@link CalendarEventSet}s</li>
+     * </ol>
+     * 
+     *  (non-Javadoc)
+     * @see org.jasig.portlet.calendar.adapter.ICalendarAdapter#getEvents(org.jasig.portlet.calendar.CalendarConfiguration, org.joda.time.Interval, javax.portlet.PortletRequest)
+     */
+    public CalendarEventSet getEvents(CalendarConfiguration calendarConfiguration,
+            Interval interval, PortletRequest request) throws CalendarException {
 
         // Some HTTP iCal providers, such as Google, don't allow you to specify
         // the interval in the RESTful call so you get the whole calendar. To
@@ -154,11 +152,11 @@ public final class ConfigurableHttpCalendarAdapter<T> extends AbstractCalendarAd
 
         // Stage 1: Try to get the cached calendar.
         String url = this.urlCreator.constructUrl(calendarConfiguration, interval, request);
-		log.debug("generated url: " + url);
-		
+        log.debug("generated url: " + url);
+
         String intermediateCacheKey = cacheKeyGenerator.getKey(calendarConfiguration, interval, request, cacheKeyPrefix.concat(".").concat(url));
 
-		T calendar;
+        T calendar;
         Element cachedCalendar = this.cache.get(intermediateCacheKey);
         if (cachedCalendar == null) {
             Credentials credentials = credentialsExtractor.getCredentials(request);
@@ -186,19 +184,19 @@ public final class ConfigurableHttpCalendarAdapter<T> extends AbstractCalendarAd
             calendar = (T) cachedCalendar.getValue();
         }
 
-		// The cache key for retrieving a calendar over HTTP may not include
-		// the interval, so we need to add the current interval to the existing
-		// cache key.  This might result in the interval being contained in the
-		// key twice, but that won't hurt anything.
-		String processorCacheKey = getIntervalSpecificCacheKey(intermediateCacheKey, interval);
+        // The cache key for retrieving a calendar over HTTP may not include
+        // the interval, so we need to add the current interval to the existing
+        // cache key.  This might result in the interval being contained in the
+        // key twice, but that won't hurt anything.
+        String processorCacheKey = getIntervalSpecificCacheKey(intermediateCacheKey, interval);
 
         // Stage 2: Get the calendar event set for the requested interval from cache
         // or generate it from the calendar from stage 1.
         CalendarEventSet eventSet;
         Element cachedElement = this.cache.get(processorCacheKey);
-		if (cachedElement == null) {
-			Set<VEvent> events = contentProcessor.getEvents(interval, calendar);
-			log.debug("contentProcessor found " + events.size() + " events");
+        if (cachedElement == null) {
+            Set<VEvent> events = contentProcessor.getEvents(interval, calendar);
+            log.debug("contentProcessor found " + events.size() + " events");
 
             // Save the calendar event set to the cache.  Calculate how long
             // this event set should survive.  We don't want this event set to
@@ -226,72 +224,72 @@ public final class ConfigurableHttpCalendarAdapter<T> extends AbstractCalendarAd
             }
             eventSet = insertCalendarEventSetIntoCache(this.cache, processorCacheKey, events,
                     timeToLiveInSeconds > 0 ? timeToLiveInSeconds : -1);
-		} else {
+        } else {
             if (log.isDebugEnabled()) {
                 log.debug("Retrieving calendar event set from cache, key:" + processorCacheKey);
             }
             eventSet = (CalendarEventSet) cachedElement.getValue();
-		}
-		
-		return eventSet;
-	}
+        }
+
+        return eventSet;
+    }
 
     protected String getIntervalSpecificCacheKey(String baseKey, Interval interval) {
-	    StringBuffer buf = new StringBuffer();
-	    buf.append(baseKey);
-	    buf.append(interval.toString());
-	    return buf.toString();
-	}
+        StringBuffer buf = new StringBuffer();
+        buf.append(baseKey);
+        buf.append(interval.toString());
+        return buf.toString();
+    }
 
-	/**
-	 * Uses Commons HttpClient to retrieve the specified url (optionally with the provided 
-	 * {@link Credentials}.
-	 * The response body is returned as an {@link InputStream}.
-	 * 
-	 * @param url URL of the calendar to be retrieved
-	 * @param credentials {@link Credentials} to use with the request, if necessary (null is ok if credentials not required)
-	 * @return the body of the http response as a stream
-	 * @throws CalendarException wraps all potential {@link Exception} types 
-	 */
-	protected InputStream retrieveCalendarHttp(String url, Credentials credentials)
-			throws CalendarException {
-		HttpClient client = new HttpClient();
-		if(null != credentials) {
-			client.getState().setCredentials(AuthScope.ANY, credentials);
-		}
-		GetMethod get = null;
+    /**
+     * Uses Commons HttpClient to retrieve the specified url (optionally with the provided 
+     * {@link Credentials}.
+     * The response body is returned as an {@link InputStream}.
+     * 
+     * @param url URL of the calendar to be retrieved
+     * @param credentials {@link Credentials} to use with the request, if necessary (null is ok if credentials not required)
+     * @return the body of the http response as a stream
+     * @throws CalendarException wraps all potential {@link Exception} types 
+     */
+    protected InputStream retrieveCalendarHttp(String url, Credentials credentials)
+            throws CalendarException {
+        HttpClient client = new HttpClient();
+        if(null != credentials) {
+            client.getState().setCredentials(AuthScope.ANY, credentials);
+        }
+        GetMethod get = null;
 
-		try {
+        try {
 
-			if(log.isDebugEnabled()) {
-				log.debug("Retrieving calendar " + url);
-			}
-			get = new GetMethod(url);
-			int rc = client.executeMethod(get);
-			if(rc == HttpStatus.SC_OK) {
-				// return the response body
-				log.debug("request completed successfully");
-				InputStream in = get.getResponseBodyAsStream();
-				ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-				IOUtils.copyLarge(in, buffer);
-				return new ByteArrayInputStream(buffer.toByteArray());
-			}
-			else {
-				log.warn("HttpStatus for " + url + ":" + rc);
-				throw new CalendarException("non successful status code retrieving " + url + ", status code: " + rc);
-			}
-		} catch (HttpException e) {
-			log.warn("Error fetching iCalendar feed", e);
-			throw new CalendarException("Error fetching iCalendar feed", e);
-		} catch (IOException e) {
-			log.warn("Error fetching iCalendar feed", e);
-			throw new CalendarException("Error fetching iCalendar feed", e);
-		} finally {
-			if (get != null) {
-				get.releaseConnection();
-			}
-		}
+            if(log.isDebugEnabled()) {
+                log.debug("Retrieving calendar " + url);
+            }
+            get = new GetMethod(url);
+            int rc = client.executeMethod(get);
+            if(rc == HttpStatus.SC_OK) {
+                // return the response body
+                log.debug("request completed successfully");
+                InputStream in = get.getResponseBodyAsStream();
+                ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+                IOUtils.copyLarge(in, buffer);
+                return new ByteArrayInputStream(buffer.toByteArray());
+            }
+            else {
+                log.warn("HttpStatus for " + url + ":" + rc);
+                throw new CalendarException("non successful status code retrieving " + url + ", status code: " + rc);
+            }
+        } catch (HttpException e) {
+            log.warn("Error fetching iCalendar feed", e);
+            throw new CalendarException("Error fetching iCalendar feed", e);
+        } catch (IOException e) {
+            log.warn("Error fetching iCalendar feed", e);
+            throw new CalendarException("Error fetching iCalendar feed", e);
+        } finally {
+            if (get != null) {
+                get.releaseConnection();
+            }
+        }
 
-	}
-	
+    }
+
 }
